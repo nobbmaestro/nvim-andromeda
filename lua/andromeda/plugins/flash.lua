025c52b0260a93c9555302e55310d90a2ffcaa6a
Norbert Batiuk <norbert@batiuk.se>
2023-09-19 13:26:52 +0200

    Update flash config

--- lua/andromeda/plugins/flash.lua
+++ lua/andromeda/plugins/flash.lua
@@ -1,6 +1,6 @@
 local Group = require("colorbuddy").Group
 local colors = require("colorbuddy").colors
 
-Group.new("FlashLabel", colors.mono_1, colors.hot_pink)
+Group.new("FlashLabel", nil, colors.hot_pink)
 Group.new("FlashMatch", colors.mono_1, colors.blue)
-Group.new("FlashBackdrop", colors.mono_1, nil)
+Group.new("FlashBackdrop", colors.mono_3, nil)
