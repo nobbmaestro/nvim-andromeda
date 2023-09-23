8de227bf7f29e449c97aaaac2b5688894cd92b44
Norbert Batiuk <norbert@batiuk.se>
2023-09-23 14:48:52 +0200

    Add support for rainbow-delimiters

---
+++ lua/andromeda/plugins/rainbow-delimiters.lua
@@ -0,0 +1,12 @@
+local cb = require("colorbuddy")
+
+local Group = cb.Group
+local colors = cb.colors
+
+Group.new("RainbowDelimiterRed", colors.red)
+Group.new("RainbowDelimiterYellow", colors.yellow)
+Group.new("RainbowDelimiterBlue", colors.blue)
+Group.new("RainbowDelimiterOrange", colors.orange)
+Group.new("RainbowDelimiterGreen", colors.green)
+Group.new("RainbowDelimiterViolet", colors.purple)
+Group.new("RainbowDelimiterCyan", colors.cyan)
