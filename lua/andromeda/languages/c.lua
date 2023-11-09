074ffab2608702de5ba7a5b7c637b7f0efa21ef4
Norbert Batiuk <norbert@batiuk.se>
2023-11-09 18:19:46 +0100

    Update property group for clang

--- lua/andromeda/languages/c.lua
+++ lua/andromeda/languages/c.lua
@@ -1,8 +1,8 @@
 local Group = require("colorbuddy").Group
 local c = require("colorbuddy").colors
 
+Group.new("@property.c", c.yellow)
 Group.new("@type.builtin.c", c.purple)
 Group.new("@type.definition.c", c.yellow)
 Group.new("@type.qualifier.c", c.yellow)
 Group.new("@variable.builtin.c", c.yellow)
-
