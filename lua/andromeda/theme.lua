494a11544f76b697bfe2adccc3f47db9bf97059e
Norbert Batiuk <norbert@batiuk.se>
2023-12-01 19:28:42 +0100

    Move style helpers to own module

--- lua/andromeda/theme.lua
+++ lua/andromeda/theme.lua
@@ -1,27 +1,7 @@
 local Group = require("colorbuddy").Group
-local styles = require("colorbuddy").styles
 
-local parseStyleOpts = function(opts)
-	return {
-		it = vim.tbl_get(opts, "styles", "italic") and styles.italic or styles.NONE,
-		bo = vim.tbl_get(opts, "styles", "bold") and styles.bold or styles.NONE,
-		ul = vim.tbl_get(opts, "styles", "underline") and styles.underline or styles.NONE,
-		uc = vim.tbl_get(opts, "styles", "undercurl") and styles.undercurl or styles.NONE,
-	}
-end
-
--- We need this function because we can't do styles.NONE + styles.NONE (we need to ensure it's only added once)
-local sumStyles = function(values)
-	local sum = styles.NONE
-
-	for _, value in ipairs(values) do
-		if value ~= styles.NONE then
-			sum = sum + value
-		end
-	end
-
-	return sum
-end
+local sumStyles = require("andromeda.utils").sumStyles
+local parseStyleOpts = require("andromeda.utils").parseStyleOpts
 
 local M = {}
 
