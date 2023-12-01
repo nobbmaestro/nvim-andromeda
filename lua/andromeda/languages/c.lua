f06bd50392a587004d41c694255cd044165cff83
Norbert Batiuk <norbert@batiuk.se>
2023-12-01 19:32:47 +0100

    Update preproc highlighting for C/C++

--- lua/andromeda/languages/c.lua
+++ lua/andromeda/languages/c.lua
@@ -1,8 +1,26 @@
 local Group = require("colorbuddy").Group
-local c = require("colorbuddy").colors
 
-Group.new("@property.c", c.yellow)
-Group.new("@type.builtin.c", c.purple)
-Group.new("@type.definition.c", c.yellow)
-Group.new("@type.qualifier.c", c.yellow)
-Group.new("@variable.builtin.c", c.yellow)
+local sumStyles = require("andromeda.utils").sumStyles
+local parseStyleOpts = require("andromeda.utils").parseStyleOpts
+
+local M = {}
+
+function M.setup(opts)
+	opts = opts or {}
+
+	local c = require("colorbuddy").colors
+	local g = require("colorbuddy").groups
+	local s = parseStyleOpts(opts)
+
+	Group.new("@property.c", c.yellow)
+	Group.new("@type.builtin.c", c.purple)
+	Group.new("@type.definition.c", c.yellow)
+	Group.new("@type.qualifier.c", c.yellow)
+	Group.new("@variable.builtin.c", c.yellow)
+
+	Group.new("@preproc.c", c.purple, nil, sumStyles({ s.it }))
+	Group.new("@preproc.cpp", c.purple, nil, sumStyles({ s.it }))
+	Group.link("@lsp.type.comment.c", g.preproc.c)
+end
+
+return M
--- lua/andromeda/languages/init.lua
+++ lua/andromeda/languages/init.lua
@@ -4,7 +4,7 @@ function M.setup(opts)
 	opts = opts or {}
 
 	require("andromeda.languages.bash")
-	require("andromeda.languages.c")
+	require("andromeda.languages.c").setup(opts)
 	require("andromeda.languages.css")
 	require("andromeda.languages.html")
 	require("andromeda.languages.python")
