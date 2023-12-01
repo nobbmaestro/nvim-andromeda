ee9bd9688cf13d17867014c47cc43fb6621d40ed
Norbert Batiuk <norbert@batiuk.se>
2023-12-01 19:32:22 +0100

    Pass opts to all modules

--- lua/andromeda/languages/init.lua
+++ lua/andromeda/languages/init.lua
@@ -1,5 +1,13 @@
-require("andromeda.languages.bash")
-require("andromeda.languages.c")
-require("andromeda.languages.css")
-require("andromeda.languages.html")
-require("andromeda.languages.python")
+local M = {}
+
+function M.setup(opts)
+	opts = opts or {}
+
+	require("andromeda.languages.bash")
+	require("andromeda.languages.c")
+	require("andromeda.languages.css")
+	require("andromeda.languages.html")
+	require("andromeda.languages.python")
+end
+
+return M
--- lua/andromeda/plugins/init.lua
+++ lua/andromeda/plugins/init.lua
@@ -1,10 +1,18 @@
-require("andromeda.plugins.flash")
-require("andromeda.plugins.fugitive")
-require("andromeda.plugins.gitgutter")
-require("andromeda.plugins.harpoon")
-require("andromeda.plugins.lazy")
-require("andromeda.plugins.lualine")
-require("andromeda.plugins.mason")
-require("andromeda.plugins.rainbow-delimiters")
-require("andromeda.plugins.signify")
-require("andromeda.plugins.telescope")
+local M = {}
+
+function M.setup(opts)
+	opts = opts or {}
+
+	require("andromeda.plugins.flash")
+	require("andromeda.plugins.fugitive")
+	require("andromeda.plugins.gitgutter")
+	require("andromeda.plugins.harpoon")
+	require("andromeda.plugins.lazy")
+	require("andromeda.plugins.lualine")
+	require("andromeda.plugins.mason")
+	require("andromeda.plugins.rainbow-delimiters")
+	require("andromeda.plugins.signify")
+	require("andromeda.plugins.telescope")
+end
+
+return M
